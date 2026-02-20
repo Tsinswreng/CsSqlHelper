5b67acb9c812fde1b06d64502872a2e00a8ca833
Tsinswreng <Tsinswreng@gmail.com>
2026-02-20 21:28:39 +0800

    Doc

--- proj/Tsinswreng.CsSqlHelper/ExtnITable.cs
+++ proj/Tsinswreng.CsSqlHelper/ExtnITable.cs
@@ -7,16 +7,15 @@ using Tsinswreng.CsPage;
 using Tsinswreng.CsTools;
 using IStr_Any = System.Collections.Generic.IDictionary<str, obj?>;
 using Str_Any = System.Collections.Generic.Dictionary<str, obj?>;
-public static partial class ExtnITable{
+public static partial class ExtnITable {
 
-	extension(ITable z){
-		[Doc("""
-		quote field name for SQL, e.g. in sqlite: "field_name"; in mysql: `field_name`;
-		"""
-		)]
+	extension(ITable z) {
+		[Doc($@"
+			Quote field name for SQL, e.g. in sqlite: ""field_name""; in mysql: `field_name`;
+			")]
 		public str Qt(
 			str s
-		){
+		) {
 			return z.SqlMkr.Quote(s);
 		}
 
@@ -26,8 +25,8 @@ public static partial class ExtnITable{
 		use fn(nameof(PoUser.UserName)) to get the {nameof(IColumn)} object of the `UserName` field.
 		]
 		")]
-		public IColumn GetCol(str CodeColName){
-			if(!z.Columns.TryGetValue(CodeColName, out var Col)){
+		public IColumn GetCol(str CodeColName) {
+			if (!z.Columns.TryGetValue(CodeColName, out var Col)) {
 				throw new Exception($"In Table {z.DbTblName}, No such column: {CodeColName}\nAvailable columns: {string.Join(", ", z.Columns.Keys)}");
 			}
 			return Col;
@@ -40,7 +39,7 @@ public static partial class ExtnITable{
 		")]
 		public str ColNameToDb(
 			str CodeColName
-		){
+		) {
 			var Col = z.GetCol(CodeColName);
 			var DbColName = Col.DbName;
 			return DbColName;
@@ -51,7 +50,7 @@ public static partial class ExtnITable{
 		")]
 		public str Fld(
 			str CodeColName
-		){
+		) {
 			var dbColName = z.ColNameToDb(CodeColName);
 			return z.SqlMkr.Quote(dbColName);
 		}
@@ -59,91 +58,138 @@ public static partial class ExtnITable{
 		[Doc(@$"
 		#Example[fn<PoUser>(x=>x.UserName) returns string UserName]
 		")]
-		public str Memb<T>(Expression<Func<T, obj?>> ExprMemb){
+		[Doc($@"
+				#Sum[Get member name from expression]
+				#Params([Expression to extract member name from])
+				#Rtn[Member name as string]
+				#Example[fn<PoUser>(x=>x.UserName) returns string ""UserName""]
+				")]
+		public str Memb<T>(Expression<Func<T, obj?>> ExprMemb) {
 			return ToolExpr.GetMemberName(ExprMemb);
 		}
 
-		//"db_col_name"  不帶表名前綴
-		public IField Fld<T>(Expression<Func<T, obj?>> ExprMemb){
+		[Doc($@"
+				#Sum[Create field reference from expression]
+				#Params([Expression to extract member name from])
+				#Rtn[IField object with table context]
+				")]
+		public IField Fld<T>(Expression<Func<T, obj?>> ExprMemb) {
 			var memberName = ToolExpr.GetMemberName<T>(ExprMemb);
 			var R = new Field(z, memberName);
 			return R;
 		}
 
-		public ISqlSplicer<T> SqlSplicer<T>(){
+		[Doc($@"
+				#Sum[Create typed SQL splicer]
+				#Rtn[Typed SQL splicer with table context set]
+				")]
+		public ISqlSplicer<T> SqlSplicer<T>() {
 			var R = new SqlSplicer<T>();
 			R.Tbl = z;
 			return R;
 		}
 
-		public ISqlSplicer SqlSplicer(){
+		[Doc($@"
+				#Sum[Create untyped SQL splicer]
+				#Rtn[Untyped SQL splicer with table context set]
+				")]
+		public ISqlSplicer SqlSplicer() {
 			var R = new SqlSplicer();
 			R.Tbl = z;
 			return R;
 		}
 
 
-		/// 映射到數據庫表ʹ字段名 並加引號/括號
-		/// IParam CodeColNameParam 其Name須同於數據庫字段名
+		[Doc($@"
+				#Sum[Map code column name to quoted database column name]
+				#Params([IParam whose Name is the code column name])
+				#Rtn[Quoted database column name]
+				")]
 		public str Fld(
 			IParam CodeColNameParam
-		){
+		) {
 			var CodeColName = CodeColNameParam.Name;
 			var DbColName = z.ColNameToDb(CodeColName);
 			return z.SqlMkr.Quote(DbColName);
 		}
 
+		[Doc($@"
+				#Sum[Create SQL parameter with specified name]
+				#Params([Parameter name])
+				")]
 		public IParam Prm(
 			str Name
-		){
+		) {
 			return z.SqlMkr.Param(Name);
 		}
 
-		/// _ 加上 64進制 ULID 如 "@_1ccGi7C87H-LETKfaB_JX"
-		public IParam Prm(){
-			//TODO 抽作獨ʹ工具
+		[Doc($@"
+				#Sum[Create SQL parameter with auto-generated unique name]
+				#Rtn[IParam with name like ""@_1ccGi7C87H-LETKfaB_JX""]
+				")]
+		public IParam Prm() {
 			var bytes = Ulid.NewUlid().ToByteArray();
 			var id = ToolUInt128.ByteArrToUInt128(bytes);
 			var Name = ToolUInt128.ToLow64Base(id);
-			return z.SqlMkr.Param("_"+Name);
+			return z.SqlMkr.Param("_" + Name);
 		}
 
+		[Doc($@"
+				#Sum[Convert database dictionary to code dictionary]
+				#Params([Dictionary with database column names as keys])
+				#Rtn[Dictionary with code column names as keys and upper-cased values]
+				")]
 		public IStr_Any ToCodeDict(
 			IStr_Any DbDict
-		){
+		) {
 			var ans = new Str_Any();
-			foreach(var (kDb, vDb) in DbDict){
+			foreach (var (kDb, vDb) in DbDict) {
 				//TODO 製函數㕥統一字典取值、取不到則友好ᵈ報錯並列有效ʹ鍵
 				var kCode = z.DbColName_CodeColName[kDb];
 				var colCode = z.Columns[kCode];
-				var vCode = colCode.RawToUpper?.Invoke(vDb)??vDb;
+				var vCode = colCode.RawToUpper?.Invoke(vDb) ?? vDb;
 				ans[kCode] = vCode;
 			}
 			return ans;
 		}
 
+		[Doc($@"
+				#Sum[Assign database values to existing entity]
+				#Params([Dictionary with database column names], [Entity to assign values to])
+				#Rtn[The assigned entity]
+				")]
 		public T AssignEntity<T>(
 			IStr_Any DbDict
-			,T ToBeAssigned
-		){
+			, T ToBeAssigned
+		) {
 			var CodeDict = z.ToCodeDict(DbDict);
 			z.DictMapper.AssignShallowT(ToBeAssigned, CodeDict);
 			return ToBeAssigned;
 		}
 
+		[Doc($@"
+				#Sum[Convert database dictionary to entity with reference]
+				#Params([Dictionary with database column names], [Reference to entity instance, will be created if null])
+				#Rtn[The entity instance]
+				")]
 		public TPo DbDictToEntity<TPo>(
 			IStr_Any DbDict
-			,ref TPo R
-		)where TPo:new(){
+			, ref TPo R
+		) where TPo : new() {
 			var CodeDict = z.ToCodeDict(DbDict);
 			R ??= new TPo();
 			z.DictMapper.AssignShallowT(R, CodeDict);
 			return R;
 		}
 
+		[Doc($@"
+				#Sum[Convert database dictionary to new entity]
+				#Params([Dictionary with database column names])
+				#Rtn[New entity instance with assigned values]
+				")]
 		public TPo DbDictToEntity<TPo>(
 			IStr_Any DbDict
-		)where TPo:new(){
+		) where TPo : new() {
 			var R = new TPo();
 			z.DbDictToEntity(DbDict, ref R);
 			return R;
@@ -151,58 +197,79 @@ public static partial class ExtnITable{
 
 		//static i32 i = 0;
 
+		[Doc($@"
+				#Sum[Convert code dictionary to database dictionary]
+				#Params([Dictionary with code column names as keys])
+				#Rtn[Dictionary with database column names as keys and raw values]
+				")]
 		public IStr_Any ToDbDict(
 			IStr_Any CodeDict
-		){
+		) {
 			var ans = new Str_Any();
-			foreach(var (kCode, vCode) in CodeDict){
+			foreach (var (kCode, vCode) in CodeDict) {
 				var Col = z.Columns[kCode];
-				var vDb = Col.UpperToRaw?.Invoke(vCode)??vCode;
+				var vDb = Col.UpperToRaw?.Invoke(vCode) ?? vCode;
 				ans[Col.DbName] = vDb;
 			}
 			return ans;
 
 		}
 
+		[Doc($@"
+				#Sum[Convert upper value to raw value for specific column]
+				#Params([Upper value], [Code column name])
+				")]
 		public obj? UpperToRaw(
 			obj? UpperValue
-			,str CodeColName
-		){
+			, str CodeColName
+		) {
 			var Col = z.GetCol(CodeColName);
-			return Col.UpperToRaw?.Invoke(UpperValue)??UpperValue;
+			return Col.UpperToRaw?.Invoke(UpperValue) ?? UpperValue;
 		}
 
+		[Doc($@"
+				#Sum[Convert upper value to raw value with type fallback]
+				#Params([Upper value], [Upper type], [Optional code column name for column-specific conversion])
+				#Rtn[Raw value after conversion, or original value if no converter found]
+				")]
 		public obj? UpperToRaw(
 			obj? UpperValue
-			,Type UpperType
-			,str? CodeColName = null
-		){
-			if(CodeColName != null
+			, Type UpperType
+			, str? CodeColName = null
+		) {
+			if (CodeColName != null
 				&& (z.Columns.TryGetValue(CodeColName, out var Col))
-			){
-				return Col.UpperToRaw?.Invoke(UpperValue)??UpperValue;
+			) {
+				return Col.UpperToRaw?.Invoke(UpperValue) ?? UpperValue;
 			}
-			if(z.UpperType_DfltMapper.TryGetValue(UpperType, out var Mapper)){
-				return Mapper.UpperToRaw?.Invoke(UpperValue)??UpperValue;
+			if (z.UpperType_DfltMapper.TryGetValue(UpperType, out var Mapper)) {
+				return Mapper.UpperToRaw?.Invoke(UpperValue) ?? UpperValue;
 			}
 			return UpperValue;
-			//throw new Exception("No UpperTypeMapperFn for type: "+ typeof(T));
 		}
 
 
+		[Doc($@"
+				#Sum[Generic version of UpperToRaw with type inference]
+				#Params([Upper value], [Optional code column name])
+				")]
 		public obj? UpperToRaw<T>(
 			T UpperValue
-			,str? CodeColName = null
-		){
+			, str? CodeColName = null
+		) {
 			return z.UpperToRaw(UpperValue, typeof(T), CodeColName);
 		}
 
+		[Doc($@"
+				#Sum[Convert raw value to upper value for specific column]
+				#Params([Raw value from database], [Code column name])
+				")]
 		public obj? RawToUpper(
 			obj? RawValue
-			,str CodeColName
-		){
+			, str CodeColName
+		) {
 			var Col = z.GetCol(CodeColName);
-			return Col.RawToUpper?.Invoke(RawValue)??RawValue;
+			return Col.RawToUpper?.Invoke(RawValue) ?? RawValue;
 		}
 
 		// public T RawToUpper<T>(//此處T非實體類 洏是列ʹ類型
@@ -214,19 +281,29 @@ public static partial class ExtnITable{
 		// }
 
 
+		[Doc($@"
+				#Sum[Convert raw value to upper value with generic return type]
+				#Params([Raw value from database], [Code column name])
+				#Rtn[Converted value cast to T]
+				")]
 		public T RawToUpper<T>(
 			obj? RawValue
-			,str CodeColName
-		){
+			, str CodeColName
+		) {
 			var Col = z.GetCol(CodeColName);
-			return (T)(Col.RawToUpper?.Invoke(RawValue)??RawValue)!;
+			return (T)(Col.RawToUpper?.Invoke(RawValue) ?? RawValue)!;
 		}
 
+		[Doc($@"
+				#Sum[Generate UPDATE clause for specified fields]
+				#Params([Enumerable of code field names to update])
+				#Rtn[SQL SET clause like ""field1 = @field1, field2 = @field2""]
+				")]
 		public str UpdateClause(
 			IEnumerable<str> UpperFields
-		){
+		) {
 			List<str> segs = [];
-			foreach(var rawField in UpperFields){
+			foreach (var rawField in UpperFields) {
 				var field = z.Fld(rawField);
 				var param = z.Prm(rawField);
 				segs.Add(field + " = " + param);
@@ -234,12 +311,17 @@ public static partial class ExtnITable{
 			return string.Join(", ", segs);
 		}
 
+		[Doc($@"
+				#Sum[Generate INSERT clause for specified fields]
+				#Params([Enumerable of code field names to insert])
+				#Rtn[SQL INSERT clause like ""(field1, field2) VALUES (@field1, @field2)""]
+				")]
 		public str InsertClause(
 			IEnumerable<str> RawFields
-		){
+		) {
 			List<str> Fields = [];
 			List<IParam> Params = [];
-			foreach(var rawField in RawFields){
+			foreach (var rawField in RawFields) {
 				var field = z.Fld(rawField);
 				var param = z.Prm(rawField);
 				Fields.Add(field);
@@ -248,12 +330,17 @@ public static partial class ExtnITable{
 			return "(" + string.Join(", ", Fields) + ") VALUES (" + string.Join(", ", Params) + ")";
 		}
 
+		[Doc($@"
+				#Sum[Generate INSERT clause for batch operations with numbered parameters]
+				#Params([Enumerable of code field names], [Number of parameter groups])
+				#Rtn[SQL INSERT clause with multiple value groups like ""(field1, field2) VALUES (@field1__0, @field2__0), (@field1__1, @field2__1)...""]
+				")]
 		public str InsertManyClause(
 			IEnumerable<str> RawFields
-			,u64 GroupCnt = 1000
-		){
+			, u64 GroupCnt = 1000
+		) {
 			IList<str> Fields = [];
-			foreach(var rawField in RawFields){
+			foreach (var rawField in RawFields) {
 				var field = z.Fld(rawField);
 				var param = z.Prm(rawField);
 				Fields.Add(field);
@@ -261,9 +348,9 @@ public static partial class ExtnITable{
 
 
 			IList<IList<IParam>> ParamLists = [];
-			for(u64 i = 0; i < GroupCnt; i++){
+			for (u64 i = 0; i < GroupCnt; i++) {
 				IList<IParam> Params = [];
-				foreach(var rawField in RawFields){
+				foreach (var rawField in RawFields) {
 					var param = z.NumFieldParam(rawField, i);
 					Params.Add(param);
 				}
@@ -272,13 +359,12 @@ public static partial class ExtnITable{
 			var sqlFieldsValus = "(" + string.Join(", ", Fields) + ") VALUES";
 
 			List<str> R = [];
-			//R.Add(sqlFieldsValus);
-			for(u64 i = 0; i < GroupCnt; i++){
+			for (u64 i = 0; i < GroupCnt; i++) {
 				var ParamList = ParamLists[i.AsI32()];
 				R.Add(" (" + string.Join(", ", ParamList) + ")");
 			}
 
-			return sqlFieldsValus+" "+string.Join(", ", R);
+			return sqlFieldsValus + " " + string.Join(", ", R);
 		}
 
 		[Doc(@$"
@@ -288,66 +374,83 @@ public static partial class ExtnITable{
 		")]
 		public IParam NumFieldParam(
 			str Field, u64 Num
-		){
-			return z.Prm(Field+"__"+Num);
+		) {
+			return z.Prm(Field + "__" + Num);
 		}
 
 		/// Num:0 -> @_0
+		[Doc($@"
+				#Sum[Create numbered parameter]
+				#Params([Number to use as suffix])
+				#Rtn[IParam with name like ""@_0"", ""@_1""]
+				")]
 		public IParam NumParam(
 			u64 Num
-		){
-			return z.Prm("_"+Num);
+		) {
+			return z.Prm("_" + Num);
 		}
 
 
-		/// (EndPos: 2, StartPos: 0) -> [@_0, @_1, @_2]
-		/// StartPos<=x<=EndPos
+		[Doc($@"
+				#Sum[Generate numbered parameters from StartPos to EndPos inclusive]
+				#Params([End position], [Start position, default 0])
+				#Rtn[List of IParam from @_StartPos to @_EndPos]
+				")]
 		public IList<IParam> NumParamsEndStart(
 			u64 EndPos, u64 StartPos = 0
-		){
+		) {
 			var R = new List<IParam>();
-			for(u64 i = StartPos; i <= EndPos; i++){
+			for (u64 i = StartPos; i <= EndPos; i++) {
 				var Param = z.NumParam(i);
 				R.Add(Param);
 			}
 			return R;
 		}
 
-		/// (StartPos: 0, EndPos: 2) -> [@_0, @_1, @_2]
-		/// StartPos<=x<=EndPos
+		[Doc($@"
+				#Sum[Generate numbered parameters from StartPos to EndPos inclusive]
+				#Params([Start position], [End position])
+				#Rtn[List of IParam from @_StartPos to @_EndPos]
+				")]
 		public IList<IParam> NumParams(
 			u64 StartPos, u64 EndPos
-		){
+		) {
 			var R = new List<IParam>();
-			for(u64 i = StartPos; i <= EndPos; i++){
+			for (u64 i = StartPos; i <= EndPos; i++) {
 				var Param = z.NumParam(i);
 				R.Add(Param);
 			}
 			return R;
 		}
 
-		[Doc(@$"
-		generate numbered params from 0 to {nameof(Cnt)}-1
-		#Example[fn(2) -> [@_0, @_1]]
-		")]
+		[Doc($@"
+				#Sum[Generate numbered parameters from 0 to Cnt-1]
+				#Params([Count of parameters])
+				#Rtn[List of IParam from @_0 to @_{Cnt - 1}]
+				#Example[fn(2) -> [@_0, @_1]]
+				")]
 		public IList<IParam> NumParams(
 			u64 Cnt
-		){
-			return z.NumParams(0, Cnt-1);
+		) {
+			return z.NumParams(0, Cnt - 1);
 		}
 
 
-		/// (@0, @1, @2 ...)
+		[Doc($@"
+				#Sum[Generate numbered parameter clause for IN clause]
+				#Params([End position], [Start position, default 0])
+				#Rtn[SQL clause like ""(@0, @1, @2)""]
+				")]
 		public str NumParamClause(
 			u64 EndPos
-			,u64 StartPos = 0
-		){
+			, u64 StartPos = 0
+		) {
 			List<str> R = [];
 			R.Add("(");
-			for(u64 i = StartPos; i <= EndPos; i++){
-				var Param = z.Prm(i+"").ToString()??"";
+			for (u64 i = StartPos; i <= EndPos; i++) {
+				var Param = z.Prm(i + "").ToString() ?? "";
 				R.Add(Param);
-				if(i == EndPos){
+				if (i == EndPos) {
 					R.Add(", ");
 				}
 			}
@@ -355,86 +458,97 @@ public static partial class ExtnITable{
 			return string.Join("", R);
 		}
 
-		[Doc(@$"
-		#Example[[@0, @1, @2 ...]]
-		")]
+		[Doc($@"
+				#Sum[Generate numbered parameters from Start to End inclusive]
+				#Params([Start number], [End number])
+				#Rtn[List of IParam from @Start to @End]
+				")]
 		public IList<IParam> Prm(
 			u64 Start
-			,u64 End
-		){
+			, u64 End
+		) {
 			var R = new List<IParam>();
-			for(u64 i = Start; i <= End; i++){
-				var Param = z.Prm(i+"");
+			for (u64 i = Start; i <= End; i++) {
+				var Param = z.Prm(i + "");
 				R.Add(Param);
 			}
 			return R;
 		}
 
-		[Doc(@$"generate sql like DbColName = @Param.Name")]
+		[Doc($@"
+				#Sum[Generate equality condition]
+				#Params([Database column name], [Parameter])
+				#Rtn[SQL equality like ""DbColName = @Param""]
+				")]
 		public str Eq(
 			str DbColName, IParam Param
-		){
+		) {
 			return z.SqlMkr.Eq(DbColName, Param);
 		}
 
-		[Doc(@$"generate sql like Param.Name = Param.ToString()")]
-		public str Eq(IParam Param){
+		[Doc($@"
+				#Sum[Generate self-equality condition]
+				#Params([Parameter whose Name is used as column name])
+				#Rtn[SQL equality like ""Param.Name = Param""]
+				")]
+		public str Eq(IParam Param) {
 			return z.Eq(Param.Name, Param);
 		}
 
 
 		[Doc(@$"generate sql for create table")]
-		public str SqlMkTbl(){
-			str OneCol(ITable Tbl, IColumn Col){
+		public str SqlMkTbl() {
+			str OneCol(ITable Tbl, IColumn Col) {
 				var R = new List<str>();
 				R.Add(Tbl.Qt(Col.DbName));
-				if(!str.IsNullOrEmpty(Col.DbType)){
+				if (!str.IsNullOrEmpty(Col.DbType)) {
 					R.Add(Col.DbType);
-				}else{
-					if(Col.RawCodeType == null){
+				}
+				else {
+					if (Col.RawCodeType == null) {
 						var Msg = $"{Col.DbName}:\nCol.RawClrType == null";
 						throw new Exception("Col.RawClrType == null");
 					}
-					try{
+					try {
 						var DbTypeName = z.SqlMkr.SqlTypeMapper.ToDbTypeName(Col.RawCodeType);
 						R.Add(DbTypeName);
 					}
-					catch (System.Exception e){
+					catch (System.Exception e) {
 						throw new Exception($"Type Mapping Error:\n{Tbl.DbTblName}.{Col.DbName}\n{e}");
 					}
 				}
-				R.AddRange(Col.AdditionalSqls??[]);
-				if(Col.NotNull){
+				R.AddRange(Col.AdditionalSqls ?? []);
+				if (Col.NotNull) {
 					R.Add("NOT NULL");
 				}
 				return string.Join(" ", R);
 			}
 
-			str FmtInnerSqls(IList<str>? Sqls){
-				if(Sqls == null || Sqls.Count == 0){
+			str FmtInnerSqls(IList<str>? Sqls) {
+				if (Sqls == null || Sqls.Count == 0) {
 					return "";
 				}
-				return $",\n\n{str.Join(",\n\t", z.InnerAdditionalSqls??[])}";
+				return $",\n\n{str.Join(",\n\t", z.InnerAdditionalSqls ?? [])}";
 			}
 
-			str FmtOuterSqls(IList<str>? Sqls){
-				if(Sqls == null || Sqls.Count == 0){
+			str FmtOuterSqls(IList<str>? Sqls) {
+				if (Sqls == null || Sqls.Count == 0) {
 					return "";
 				}
 				List<str> R = [];
-				foreach(var sql in Sqls){
+				foreach (var sql in Sqls) {
 					R.Add(sql);
 					R.Add(";\n");
 				}
-				return str.Join("",R);
+				return str.Join("", R);
 			}
 
 			var Lines = new List<str>();
-			foreach(var (name, Col) in z.Columns){
+			foreach (var (name, Col) in z.Columns) {
 				Lines.Add(OneCol(z, Col));
 			}
-	var S =
-	$"""
+			var S =
+			$"""
 	CREATE TABLE IF NOT EXISTS {z.Qt(z.DbTblName)}(
 		{string.Join(",\n\t", Lines)}{FmtInnerSqls(z.InnerAdditionalSqls)}
 	);
@@ -444,21 +558,26 @@ public static partial class ExtnITable{
 		}
 
 
-		[Doc(@$"Set {nameof(PageQry.PageSize)} to MaxValue, not real select all")]
-		public IPageQry PageSlctAll(){
+		[Doc($@"
+				#Sum[Create page query that selects all records]
+				#Desc[Sets PageSize to MaxValue, not a real SELECT ALL]
+				")]
+		public IPageQry PageSlctAll() {
 			var R = new PageQry();
-			if(z.TblMgr?.DbSrcType == ConstDbSrcType.Postgres){
+			if (z.TblMgr?.DbSrcType == ConstDbSrcType.Postgres) {
 				R.PageSize = i64.MaxValue;
-			}else{
+			}
+			else {
 				R.PageSize = u64.MaxValue;
 			}
 			return R;
 		}
+
 	}
 }
 
 #if DEBUG
-public class Dbg{
+public class Dbg {
 	public static i64 Cnt = 0;
 }
 #endif
