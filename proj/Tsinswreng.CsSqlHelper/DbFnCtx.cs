c1f4b889e4dc3db48db060819dce969b7b5fc5bb
Tsinswreng <Tsinswreng@gmail.com>
2026-03-01 21:20:16 +0800

    潙DbFnCtx作文檔

--- proj/Tsinswreng.CsSqlHelper/DbFnCtx.cs
+++ proj/Tsinswreng.CsSqlHelper/DbFnCtx.cs
@@ -3,10 +3,16 @@ namespace Tsinswreng.CsSqlHelper;
 using System.Data;
 
 
-
+[Doc(@$"Database Function Context")]
 public partial class DbFnCtx:IDbFnCtx{
+	[Doc(@$"Transaction")]
 	public ITxn? Txn{get;set;}
+	[Doc(@$"don't need to set {nameof(DbConn)} manually
+	when you pass DbFnCtx to {nameof(ISqlCmdMkr.MkCmd)}
+	if {nameof(DbConn)} is null, it will be initialized by {nameof(IDbConnMgr)}
+	")]
 	public IDbConnection? DbConn{get;set;}
+	[Doc(@$"KV Properties")]
 	public IDictionary<str, object?>? Props{get;set;}
 	public ICollection<obj?>? ObjsToDispose{get;set;}
 #if Impl
